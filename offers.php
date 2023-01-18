15f953906876e8947c0e1a26c5ce9d4dd43f4cb3
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2023-01-18 02:56:51 +0100

    offers page rendering data from DB using bladeOne

--- offers.php
+++ offers.php
@@ -1,4 +1,12 @@
 <?php
 include "utils/blade_connection.php";
-echo $blade->run("offers");
+include "db/connection.php";
+
+$sql = "SELECT * FROM rooms WHERE offerPrice IS NOT NULL";
+$result = $conn->query($sql);
+$sql_all_rooms = "SELECT * FROM rooms";
+$result_all_rooms = $conn->query($sql_all_rooms);
+
+echo $blade->run("offers", array("offer_rooms" => $result->fetch_all(MYSQLI_ASSOC), "rooms" => $result_all_rooms->fetch_all(MYSQLI_ASSOC)));
+
 ?>
